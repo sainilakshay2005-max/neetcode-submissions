0ba5b0be248b4390a843f1aad70cb6005964f5d1
sainilakshay2005-max <sainilakshay2005@gmail.com>
2026-09-24 11:51:25 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_map<int,int> m;
+       for(int x:nums){
+        m[x]++;
+
+        if(m[x]>1){
+            return true;
+        }
+       }
+       return false;
+        
+    }
+};
